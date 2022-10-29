bb3f5c41692102da4685920278aafe0ed226c0fb
emanuelegurini <emanuele.gurini89@gmail.com>
2022-10-29 21:55:45 +0200

    style: :art: ADD style to the Header components

--- src/pages/Home/components/Card/Card.jsx
+++ src/pages/Home/components/Card/Card.jsx
@@ -20,6 +20,7 @@ const Card = ({ country }) => {
   };
 
   if (country) {
+    console.log(country);
     Object.assign(data, {
       cca3: country?.cca3 ? country?.cca3 : "",
       flag: country?.flags?.png,
--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import style from "./Header.module.css";
 import { ThemeContext } from "../../context/ThemeContext";
 
+import iconLight from "../../../assets/icons/icon-light-theme.svg";
+import iconDark from "../../../assets/icons/icon-dark-theme.svg";
+
 const Header = () => {
   // Get the ThemeContext and store his value inside variables by destructuring
   const { theme, setTheme } = React.useContext(ThemeContext);
@@ -16,11 +19,22 @@ const Header = () => {
     setTheme(!theme);
   };
 
+  let themeMode = theme ? "light" : "dark";
+
   return (
-    <section className={style.header}>
+    <section className={`${style.header} ${style[`header--${themeMode}`]}`}>
       <div className={style.container}>
         <h1>Where in the world?</h1>
-        <button onClick={handleTheme}>{theme ? "Dark" : "Light"}</button>
+
+        {/* THEME BUTTON */}
+        <button
+          onClick={handleTheme}
+          className={`${style[`button--${themeMode}`]}`}
+        >
+          {/* THEME ICON BUTTON */}
+          <img src={theme ? iconLight : iconDark} />
+          {theme ? "Dark" : "Light"} Mode
+        </button>
       </div>
     </section>
   );
